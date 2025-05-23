88629d80a0bb4e6800c3002913362194d0139257
gogo <bd091226@naver.com>
2025-05-23 15:49:13 +0900

    [NHJ] 컨베이어 벨트 + 목적지 도착 송수신 기능
    A차 컨베이어 벨트에 있는 버튼을 누르는 동안 컨베이너 벨트가 작동되는 기능 추가 및 A차 출발 후 3초 뒤 보관함에게 목적지 도착이라는 문구 송신하는 기능 추가

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import os
 BASE_DIR = "/home/pi/FinalProject/Final_Project"
 camera_path = os.path.join(BASE_DIR, "camera_final.py")
 button_path = os.path.join(BASE_DIR, "buttoncontrol.py")
-# camera_streamer.py와 button_publisher.py를 동시에 실행
+# dest_path = os.path.join(BASE_DIR, "container_dest.py") -> 3초후 동작하는 "목적지 도착"이라는 문구를 보내는 것이면 없어도 되지만 일단 넣어줌
+# camera_streamer.py와 button_apublisher.py를 동시에 실행
 camera_proc = subprocess.Popen(['python3', camera_path])
 button_proc = subprocess.Popen(['python3', button_path])
+#dest_proc = subprocess.Popen(['python3', dest_path])
+
 
 try:
     print("실행 중...")
@@ -17,6 +20,8 @@ except KeyboardInterrupt:
     print("\n 종료 요청 받음. 프로세스 종료 중...")
     camera_proc.terminate()
     button_proc.terminate()
+    # dest_proc.terminate()
     camera_proc.wait()
     button_proc.wait()
+    # dest_proc.wait()
     print("종료")
