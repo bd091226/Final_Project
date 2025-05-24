492c06aa8e3f02d535055961b031424aedcc3e3a
gogo <bd091226@naver.com>
2025-05-24 15:19:29 +0900

    [NHJ] main.py 정리

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import os
 BASE_DIR = "/home/pi/FinalProject/Final_Project"
 camera_path = os.path.join(BASE_DIR, "camera_final.py")
 button_path = os.path.join(BASE_DIR, "buttoncontrol.py")
-# dest_path = os.path.join(BASE_DIR, "container_dest.py") -> 3초후 동작하는 "목적지 도착"이라는 문구를 보내는 것이면 없어도 되지만 일단 넣어줌
+
 # camera_streamer.py와 button_apublisher.py를 동시에 실행
 camera_proc = subprocess.Popen(['python3', camera_path])
 button_proc = subprocess.Popen(['python3', button_path])
-#dest_proc = subprocess.Popen(['python3', dest_path])
 
 
 try:
