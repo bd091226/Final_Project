cd9887c3c66978aa6d52692cd166f8378eb91758
gogo <bd091226@naver.com>
2025-05-25 21:24:24 +0900

    [NHJ] C언어로 바꾸는 중(count 값 수신)

---
+++ changer_c/count_mattcheck.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <MQTTClient.h>
+
+#define ADDRESS     "tcp://broker.hivemq.com:1883"
+#define CLIENTID    "RaspberryPi_Container"   // 다른 클라이언트 ID 사용 권장
+#define TOPIC       "storage/count"
+#define QOS         1
+#define TIMEOUT     10000L
+
+MQTTClient client;
+
+volatile int connected = 0;
+
+void delivered(void *context, MQTTClient_deliveryToken dt) {
+    // 메시지 발송 완료 콜백 (필요시 사용)
+}
+
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+    char* payloadptr = (char*)message->payload;
+
+    // 수신 메시지를 문자열로 복사
+    char msgPayload[message->payloadlen + 1];
+    memcpy(msgPayload, payloadptr, message->payloadlen);
+    msgPayload[message->payloadlen] = '\0';
+
+    printf("Received message on topic %s: %s\n", topicName, msgPayload);
+
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+
+    return 1;
+}
+
+void connlost(void *context, char *cause) {
+    printf("Connection lost: %s\n", cause);
+    connected = 0;
+}
+
+int main(int argc, char* argv[]) {
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    int rc;
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID,
+                      MQTTCLIENT_PERSISTENCE_NONE, NULL);
+
+    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
+
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
+        printf("Failed to connect, return code %d\n", rc);
+        return -1;
+    }
+
+    printf("Connected to MQTT broker, subscribing to topic: %s\n", TOPIC);
+    MQTTClient_subscribe(client, TOPIC, QOS);
+
+    // 메시지 수신을 계속 대기 (무한 루프)
+    while(1) {
+        // sleep 등으로 CPU 점유율 낮추기
+        #ifdef _WIN32
+            Sleep(1000);
+        #else
+            sleep(1);
+        #endif
+    }
+
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+
+    return 0;
+}
