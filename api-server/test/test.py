a9b89c918c1fda45c798f484689cf79db8f4440e
baboyiban <chl11wq12@gmail.com>
2025-05-28 12:18:02 +0900

    add: test folder

--- api-server/test/test.py
+++ api-server/test/test.py
@@ -2,11 +2,10 @@ import unittest
 import requests
 
 
-class TestUsersAPI(unittest.TestCase):
-    base_url = 'https://koreanjson.com'
+class TestAPI(unittest.TestCase):
+    base_url = 'http://localhost:8080/api'
 
-    def test_get_users(self):
-        endpoint = '/users'
+    def test_get(self, endpoint):
         response = requests.get(self.base_url + endpoint)
 
         # TC1. 응답값이 200 status인 지 확인
@@ -19,11 +18,10 @@ class TestUsersAPI(unittest.TestCase):
             self.fail("Response was not in JSON format.")
 
         # TC3. 사용자 목록이 비어 있지 않은 지 확인
-        self.assertTrue(len(data) > 0, "User list is empty.")
-
-    def test_get_users_id(self):
-        endpoint = '/users/1'
-        response = requests.get(self.base_url + endpoint)
+        self.assertTrue(len(data) > 0, "List is empty.")
+        
+    def test_get_id(self, endpoint, id):
+        response = requests.get(self.base_url + endpoint + "/" + id)
 
         # TC1. 응답값이 200 status인 지 확인
         self.assertEqual(response.status_code, 200, "Status code shoud be 200.")
@@ -36,7 +34,6 @@ class TestUsersAPI(unittest.TestCase):
 
         # TC3. 사용자 목록이 비어있지 않은 지 확인
         self.assertTrue(len(data) > 0, "User list is empty.")
-
 
 class TestTodosAPI(unittest.TestCase):
     base_url = 'https://koreanjson.com'
