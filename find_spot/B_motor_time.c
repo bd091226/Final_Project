c788e462938294027addb81d09cceec4134a498a
jangdokdae <jang99046377@gmail.com>
2025-06-09 09:36:32 +0100

    [JHK] A,B 방향 작동 확인 완료

--- find_spot/B_motor_time.c
+++ find_spot/B_motor_time.c
@@ -80,22 +80,22 @@
  // Rotate one 90° step: +1 right, -1 left
  static void rotate_one(Direction *d, int turn_dir) {
      // micro forward before rotation
-     double micro = (PRE_ROTATE_FORWARD_CM / 30.0) * SECONDS_PER_GRID_STEP;
-     motor_go(); delay_sec(micro); motor_stop();
-     delay_sec(0.1);
-     if (turn_dir > 0) motor_right();
-     else             motor_left();
-     delay_sec(SECONDS_PER_90_DEG_ROTATION);
-     motor_stop();
+    //  double micro = (PRE_ROTATE_FORWARD_CM / 30.0) * SECONDS_PER_GRID_STEP;
+    //  motor_go(); delay_sec(micro); motor_stop();
+    //  delay_sec(0.1);
+    //  if (turn_dir > 0) motor_right();
+    //  else             motor_left();
+    //  delay_sec(SECONDS_PER_90_DEG_ROTATION);
+    //  motor_stop();
      int nd = (*d + turn_dir + 4) % 4;
      *d = (Direction)nd;
  }
  
  // Move forward one grid cell
  static void forward_one(void) {
-     motor_go();
-     delay_sec(SECONDS_PER_GRID_STEP);
-     motor_stop();
+    //  motor_go();
+    //  delay_sec(SECONDS_PER_GRID_STEP);
+    //  motor_stop();
      switch (pos.dir) {
          case NORTH: pos.row -= 1; break;
          case SOUTH: pos.row += 1; break;
