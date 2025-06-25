0cd9054c1766d121337f2ad1af4b0d495583a418
jangdokdae <jang99046377@gmail.com>
2025-06-25 23:55:48 +0900

    [JHK] signal include 추가, sigint 선언 등

--- changer_c/sensor.h
+++ changer_c/sensor.h
@@ -4,17 +4,22 @@
 #define SENSOR_COUNT 4
 
 #include <stdbool.h>
+#include <signal.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 // 기존 함수 선언
+void handle_sigint(int signo);
 void sensor_init(void);
 void sensor_read_distances(float distances[SENSOR_COUNT]);
 void sensor_activate_servo(int idx);
 void sensor_cycle(void);
 void sensor_cleanup(void);
+
+// 센서 쓰레드가 계속 돌아갈지 제어하는 플래그
+static volatile bool sensor_thread_running;
 
 // 새로 추가: 임계값 기반 모니터링 루프
 //  - dist_thresh: 거리 조건 (cm 이하)
