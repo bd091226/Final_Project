492bec41aac41a44e73a2e7d4f39f346f1faf135
jangdokdae <jang99046377@gmail.com>
2025-06-25 20:11:03 +0900

    [JHK] 선언 몰아주기해서 주석처리함 (encoder.h에 넣음)

--- moter_control.h
+++ moter_control.h
@@ -23,17 +23,17 @@ typedef enum
 #define MIN_PULSE_WIDTH 1000  // 0도
 #define MID_PULSE_WIDTH 2000  // 90도
 
-extern struct gpiod_line *in1_line;
-extern struct gpiod_line *in2_line;
-extern struct gpiod_line *ena_line;
-extern struct gpiod_line *in3_line;
-extern struct gpiod_line *in4_line;
-extern struct gpiod_line *enb_line;
-extern struct gpiod_line *line_btn;
-extern struct gpiod_line *servo_line;
+// extern struct gpiod_line *in1_line;
+// extern struct gpiod_line *in2_line;
+// extern struct gpiod_line *ena_line;
+// extern struct gpiod_line *in3_line;
+// extern struct gpiod_line *in4_line;
+// extern struct gpiod_line *enb_line;
+// extern struct gpiod_line *line_btn;
+// extern struct gpiod_line *servo_line;
 
-extern struct gpiod_line *trig_line;
-extern struct gpiod_line *echo_line;
+// extern struct gpiod_line *trig_line;
+// extern struct gpiod_line *echo_line;
 typedef struct
 {
     int x;
@@ -52,7 +52,6 @@ void setup();
 void cleanup();
 void set_speed(int speedA, int speedB);
 float get_distance_cm();
-void generate_pwm(struct gpiod_line *line, int pulse_width_us, int duration_ms);
 
 Direction move_step(Position curr, Position next, Direction current_dir);
 int load_path_from_file(const char *filename, Position path[]);
